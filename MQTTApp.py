7e874ac7ba2bd41c731230ab90955d9e2cc4a9fe
user <david.king.lr@gmail.com>
2020-07-26 13:49:40 -0500

    Draft 1 from desk to github to later pi

--- MQTTApp.py
+++ MQTTApp.py
@@ -1,1 +1,101 @@
-# test
+# This will communicate with Mosquitto,
+# and write into
+# main and short datatable
+
+import pymysql as mdb
+import paho.mqtt.client as mqtt
+import time
+
+dataBaseName = 'DataLogger'
+dataBaseTable = 'OURWEATHERTable'
+username = 'datalogger'
+password = 'Data0233'
+
+hostname = 'localhost'
+user_data_tuple = (hostname, username, password, dataBaseName)
+# read old data
+
+# connection1 = mdb.connect(hostname, username, password, dataBaseName)
+# my_cursor = connection1.cursor()
+
+# query = 'SELECT id, TimeStamp, deviceid, Outdoor_Temperature, Outdoor_Humidity, Barometric_Pressure, ' \
+#       'Current_Wind_Speed, Current_Wind_Gust, Current_Wind_Direction, Wind_Speed_Maximum, Wind_Gust_Maximum, ' \
+#      'OurWeather_DateTime, Lightning_Time, Lightning_Distance, Lightning_Count, Rain_Total, Rain_Now FROM ' + \
+#       dataBaseTable + ' WHERE id > 35570 '
+
+# my_cursor.execute(query)
+# result = my_cursor.fetchall()
+
+# print(result)
+
+
+broker_url = '192.168.1.84'
+broker_port = 1883
+
+
+def on_message(self, userdata, message):
+    """This function triggered when message received,
+    it will parse it into a list of data and
+    sends data_list to be writen to DB
+    """
+    full_payload = message.payload.decode()
+    index = full_payload.index("MQTT")
+    data_string = full_payload[index + 18:-2]
+    data_list = data_string.split(',')  # split the string to a list
+
+    print(data_list)
+    write_to_data(data_list)
+
+
+
+def mqtt_client():
+    client = mqtt.Client(client_id='myweather2', clean_session=True, userdata=None, transport='tcp')
+    client.on_message = on_message
+    client.connect(broker_url, broker_port)
+    client.subscribe('OurWeather')
+    client.on_connect = on_connect
+    client.loop_start()
+
+
+def on_connect(self, userdata, flags, rc):
+    print("Connected")
+
+
+def write_to_data(list_to_write):
+    """
+    Writes data to the database table
+    :param list_to_write: a list if data
+    :type list_to_write:
+    :return:
+    :rtype:
+    """
+
+    try:
+        db_connection = mdb.connect(hostname, username, password, dataBaseName)
+        my_cursor = db_connection.cursor()
+    except:
+        print("fail to connect")
+
+    device_id = 6
+    try:
+        query = 'INSERT INTO ' + dataBaseTable + (
+            ' (timestamp, deviceid, Outdoor_Temperature, Outdoor_Humidity, Barometric_Pressure, Current_Wind_Speed, '
+            'Current_Wind_Gust, Current_Wind_Direction, Wind_Speed_Maximum, Wind_Gust_Maximum, '
+            'OurWeather_DateTime, Lightning_Time, Lightning_Distance, Lightning_Count, Rain_Total, '
+            'Rain_Now) VALUES (CURRENT_TIMESTAMP, %i, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, "%s", "%s", '
+            '%i, %i, %.2f, %.3f)' % (
+                int(device_id), float(list_to_write[0]), float(list_to_write[1]), float(list_to_write[2]),
+                float(list_to_write[3]), float(list_to_write[4]), float(list_to_write[5]), float(list_to_write[7]),
+                float(list_to_write[8]), list_to_write[9], list_to_write[10], int(list_to_write[11]),
+                int(list_to_write[12]), float(list_to_write[6]), float(list_to_write[13])))
+        print(query)
+        my_cursor.execute(query)
+        db_connection.commit()
+    except:
+        print("fail to write")
+
+
+mqtt_client()
+
+while True:
+    time.sleep(60)
---
+++ TestGraph.py
@@ -0,0 +1,83 @@
+import gc
+import matplotlib
+# matplotlib.use('Agg')
+from matplotlib import pyplot
+from matplotlib import dates
+from matplotlib.ticker import MultipleLocator
+from matplotlib.ticker import FormatStrFormatter
+import pylab
+import numpy as np
+from numpy import mean
+import sys
+from pytz import timezone
+from httplib2 import http
+from datetime import datetime
+import pymysql as mdb
+import scipy
+from scipy import signal
+
+dataBaseName = 'DataLogger'
+dataBaseTable = 'OURWEATHERTable'
+username = 'datalogger'
+password = 'Data0233'
+hostname = 'localhost'
+
+db_connection = mdb.connect(hostname, username, password, dataBaseName)
+cursor = db_connection.cursor()
+
+query = 'SELECT Date, Temp, HI FROM SevenDayTempHeatIndex ORDER BY Date ASC'
+
+try:
+    cursor.execute(query)
+    result = cursor.fetchall()
+except:
+    e = sys.exc_info()[0]
+    print(f"the error is {e}")
+
+time = []
+temperature = []
+heat_index = []
+
+for record in result:
+    time.append(record[0])
+    temperature.append(record[1])
+    heat_index.append(record[2])
+
+
+
+
+
+fds = [dates.date2num(d) for d in time]
+hfmt = dates.DateFormatter('%m/%d - %H')
+
+x = np.array([fds])
+y = np.array([heat_index])
+
+x = x[y > 80]  # filter out if heat_index is less than 80
+y = y[y > 80]
+
+
+
+
+fig, ax = pyplot.subplots()
+pyplot.xticks(rotation='45')
+ax.xaxis.set_major_formatter(hfmt)
+ax.plot(fds, temperature, marker='o', linestyle='--', color='blue', markersize=2.0)
+# ax.plot(fds, heat_index, c='r')
+ax.plot(x, y, marker='o', linestyle='', color='orange', markersize=2.0)
+ax.axis(ymin=10, ymax=110)
+
+print(temperature[-1])
+print(max(temperature))
+
+# ax.annotate('local max', xy=(0.8, 0.2), xycoords='axes fraction', xytext=(0.1, 0.1), textcoords='axes fraction', arrowprops=dict(facecolor='black', shrink=0.05), horizontalalignment='right', verticalalignment='top',)
+ax.set_title("Temperature and Heat Index")
+pyplot.figtext(0.15, 0.85, f"Temperature now:\n {temperature[-1]:.1f}", horizontalalignment='left', verticalalignment='top')
+pyplot.figtext(0.75, 0.85, f"Max: {max(temperature):.1f} \nMin: {min(temperature):.1f} \nAve: {mean(temperature):.1f}", horizontalalignment='left', verticalalignment='top')
+
+ax.set_xlabel("Date")
+ax.set_ylabel("degree F")
+ax.grid(which='both', axis='both')
+pyplot.savefig('/var/www/html/TestGraph.png')
+
+pyplot.show()
